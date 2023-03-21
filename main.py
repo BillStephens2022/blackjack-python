14578e6c2d094e275d4b37bf24b8b37d25f5e1d6
BillStephens2022 <two4onebill@yahoo.com>
2023-03-21 15:39:46 -0400

    added function to compare user and computer score and display a message to the user upon completion of the game

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import random
 from art import logo
 
 # function to deal a random card
-
-
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 # function to calculate the score
-
-
 def calculate_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
     if sum(cards) == 21 and len(cards) == 2:
@@ -23,6 +19,24 @@ def calculate_score(cards):
         cards.append(1)
 
     return sum(cards)
+
+# function to compare user's and computer's score and return a message to display
+def compare(user_score, computer_score):
+  """Compares User's score with the Computer's score."""
+  if user_score == computer_score:
+    return "Draw"
+  elif computer_score == 0:
+    return "Lose, opponent has Blackjack."
+  elif user_score == 0:
+    return "Win with a Blackjack"
+  elif user_score > 21:
+    return "You went over. You lose."
+  elif computer_score > 21:
+    return "Opponent went over. You win."
+  elif user_score > computer_score:
+    return "You win"
+  else:
+    return "You lose"
 
 
 # lists to hold cards dealt for both user and computer
@@ -56,4 +70,5 @@ while computer_score <= 0 and computer_score < 17:
 
 print(f"  Your final hand: {user_cards}, final score: {user_score}")
 print(f"  Computer's final hand: {computer_cards}, final score: {computer_score}")
+print(compare(user_score, computer_score))
 
